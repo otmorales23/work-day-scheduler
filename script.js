5ff56e2107f3173e6e59c80cfe8d5021d3194b8b
otmorales23 <oliverwills0227@gmail.com>
2023-08-03 18:56:40 -0700

    able to save user input and time block to local storage

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ getTime()
 
 //save value of text area for each timeblock to local storage
 $(".saveBtn").click(function (){
-    
+    let timeblockHourId = $(this).parent().attr("id")
+    let userInput = $(this).prev().val()
+    console.log(timeblockHourId);
+    console.log(userInput);
+    localStorage.setItem(timeblockHourId, userInput)
 })
 
 //set color based on past present or future
